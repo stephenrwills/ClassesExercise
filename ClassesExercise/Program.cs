79635bde8430ed2181185b7a843ee4cdc968f607
Stephen Wills <stephenrwills94@gmail.com>
2022-07-13 14:27:42 -0500

    finished main method

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,8 @@
+﻿using ClassesExercise;
+
+var car1 = new Car();
+car1.Make = "Toyota";
+car1.Year = 2010;
+car1.Model = "Camry";
+
+Console.WriteLine($"I drive a {car1.Year} {car1.Make} {car1.Model}");  
